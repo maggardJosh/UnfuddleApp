a4eff4490575973166eeb9022ee6cfd94f295647
Josh Maggard <jmaggard62@gmail.com>
2014-03-07 00:18:36 -0600

    jquery mobile started

--- www/js/index.js
+++ www/js/index.js
@@ -1,49 +1,99 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicity call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
 
-        console.log('Received Event: ' + id);
-    }z
-};
+var domain = window.localStorage.getItem("unfuddleDomain");
+var username = window.localStorage.getItem("username");
+var password = window.localStorage.getItem("password");
+
+document.addEventListener("deviceready", onDeviceReady, false);
+$(document).ready(onDeviceReady);
+function onDeviceReady() {
+	$("#txtDomain").val(domain);
+	$("#txtUsername").val(username);
+	$("#txtPassword").val(password);
+}
+function login() {
+	window.localStorage.setItem("unfuddleDomain", $("#txtDomain").val());
+	window.localStorage.setItem("username", $("#txtUsername").val());
+	window.localStorage.setItem("password", $("#txtPassword").val());
+	$.ajax({
+		type : "GET",
+		url : "https://" + domain + ".unfuddle.com/api/v1/projects.json",
+		headers : {
+			"Authorization" : "Basic " + btoa(username + ':' + password),
+			"Accept" : "application/json"
+		},
+		timeout : 5000,
+		contentType : "json",
+		success : function (data) {
+			$("#projectList").empty().append("<li data-role='list-divider'>Projects</li>");
+			console.log(data);
+			data.forEach(function (project) {
+				$("#projectList").append("<li><a onclick='gotoProject(" + project.id + ", \"" + project.title + "\");'>" + project.title + "</a></li>");
+
+			});
+
+			$.mobile.changePage("#projectsPage");
+			$("#projectList").listview("refresh");
+		},
+		complete : function () {},
+		error : function (e) {
+			console.log(e);
+		}
+	});
+}
+
+function sortByPriority(a, b) {
+	var aPriority = a.priority;
+	var bPriority = b.priority;
+	return ((aPriority > bPriority) ? -1 : ((aPriority < bPriority) ? 1 : 0));
+}
+
+function gotoProject(projectID, projectTitle) {
+	$("#projectPageHeader").html(projectTitle);
+	$.ajax({
+		type : "GET",
+		url : "https://" + domain + ".unfuddle.com/api/v1/projects/" + projectID + "/tickets.json",
+		headers : {
+			"Authorization" : "Basic " + btoa(username + ':' + password),
+			"Accept" : "application/json"
+		},
+		timeout : 5000,
+		contentType : "json",
+		success : function (data) {
+			$("#ticketList").empty();
+			console.log(data);
+			var ticketArray = [];
+			data.forEach(function (ticket) {
+				if (ticket.status != "closed")
+					ticketArray.push(ticket);
+			});
+			ticketArray.sort(sortByPriority);
+			var ticketPriority = -1;
+			var priorityColor = "";
+			ticketArray.forEach(function (ticket) {
+				if (ticketPriority != ticket.priority) 
+				{
+					ticketPriority = ticket.priority;
+					var priorityLabel = "";
+					switch(ticketPriority)
+					{
+					case "5": priorityLabel = "Highest"; priorityColor="rgba(255,50,50,.55);"; break;
+					case "4": priorityLabel = "High"; priorityColor="rgba(255,130,130,.55);"; break;
+					case "3": priorityLabel = "Normal"; priorityColor="rgba(255,255,255,.2);"; break;
+					case "2": priorityLabel = "Low"; priorityColor="rgba(130,160,205,.5);"; break;
+					case "1": priorityLabel = "Lowest"; priorityColor="rgba(70,70,185,.3);"; break;
+					}
+					$("#ticketList").append("<li data-role='list-divider'>" + priorityLabel + "</li>");
+				}
+				$("#ticketList").append("<li style='background:"+priorityColor+"'><a>" + ticket.summary + "</a></li>");
+			});
+
+			$.mobile.changePage("#projectPage");
+			$("#ticketList").listview("refresh");
+		},
+		complete : function () {},
+		error : function (e) {
+			console.log(e);
+		}
+	});
+}
---
+++ www/js/main.js
@@ -0,0 +1,47 @@
+app.initialize();
+//window.onload = onsDeviceReady;
+/*function onsDeviceReady()
+{
+$("#txtDomain").val(domain);
+$("#txtUsername").val(username);
+$("#txtPassword").val(password);
+}*/
+var domain = window.localStorage.getItem("unfuddleDomain");
+var username = window.localStorage.getItem("username");
+var password = window.localStorage.getItem("password");
+function doThings() {
+	window.localStorage.setItem("unfuddleDomain", $("#txtDomain").val());
+	window.localStorage.setItem("username", $("#txtUsername").val());
+	window.localStorage.setItem("password", $("#txtPassword").val());
+
+	domain = window.localStorage.getItem("unfuddleDomain");
+	username = window.localStorage.getItem("username");
+	password = window.localStorage.getItem("password");
+
+	$.ajax({
+		type : "GET",
+		url : "https://" + domain + ".unfuddle.com/api/v1/projects/7/tickets.json",
+		headers : {
+			"Authorization" : "Basic " + btoa(username + ':' + password),
+			"Accept" : "application/json"
+		},
+		timeout : 5000,
+		contentType : "json",
+		success : function (data) {
+
+			console.log(data);
+			var resultingString = "";
+			data.forEach(function (ticket) {
+				if (ticket.status != "closed")
+					resultingString = resultingString.concat(ticket.summary + "<br/>");
+			});
+
+			$("#resultDiv").html(resultingString);
+		},
+		complete : function () {},
+		error : function (e) {
+			console.log(e);
+		}
+	});
+
+}
